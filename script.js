bd60215b1c32b76e2ac8e503df1d04a6adee4129
Bogdan5ik <baggi.juk@mail.ru>
2025-04-03 20:51:04 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector('.box').onclick = function() {
+    document.querySelector('.box').style.left = "100px";
+}
+
+document.querySelector('.box-1').onclick = function() {
+    document.querySelector('.box-1').style.left = "200px";
+    document.querySelector('.box-1').style.top = "-100px";
+}
+
+document.querySelector('.box-2').onclick = function() {
+    document.querySelector('.box-2').style.left = "300px";
+    document.querySelector('.box-2').style.top = "-200px";
+}
